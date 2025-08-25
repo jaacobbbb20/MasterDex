d3983c02d502de93d2e3058639096b9302d0cc1e
Jacob Hathaway <124851843+jaacobbbb20@users.noreply.github.com>
2025-08-25 16:44:45 -0700

    Update adab545bdd0a_add_missing_binder_set_id_foreign_key.py

--- migrations/versions/adab545bdd0a_add_missing_binder_set_id_foreign_key.py
+++ migrations/versions/adab545bdd0a_add_missing_binder_set_id_foreign_key.py
@@ -17,15 +17,7 @@ depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table("binders", schema=None) as batch_op:
-        batch_op.create_foreign_key(
-            "fk_binders_set_id",
-            "sets",
-            ["set_id"],
-            ["id"]
-        )
+    pass
 
 def downgrade():
-    with op.batch_alter_table("binders", schema=None) as batch_op:
-        batch_op.drop_constraint("fk_binders_set_id", type_="foreignkey")
-#nhgjgk
+    pass
